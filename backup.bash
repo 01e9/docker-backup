acf3e6e85186117f2ee145edece6c028d9bbf545
Oleg Artene <hi@01eg.art>
2019-08-17 00:18:52 +0300

    backup.bash: start containers in reversed order

--- backup.bash
+++ backup.bash
@@ -3,6 +3,7 @@
 set -e
 
 CONTAINERS="${1}"
+CONTAINERS_REVERSED=$(echo ${CONTAINERS} | tr ' ' '\n' | tac | tr '\n' ' ')
 VOLUMES="${2}"
 ARCHIVE_NAME=backup-$(date '+%d_%m_%Y-%H_%M_%S').tar.gz
 
@@ -25,7 +26,7 @@ docker run \
   busybox \
   sh -c "cd /volumes && tar -czf /tmp/${ARCHIVE_NAME} * && mv /tmp/${ARCHIVE_NAME} /backup/archives/"
 
-docker start ${CONTAINERS}
+docker start ${CONTAINERS_REVERSED}
 
 find /backup/archives -maxdepth 1 -mtime +30 -type f -delete
 
